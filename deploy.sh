e279b78f1c88464838de056bb68ca1136c58c4dd
Stanislav <stasyatr@gmail.com>
2024-01-24 21:51:04 +0300

    (Init) started new version

--- deploy.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-export ANSIBLE_HOST_KEY_CHECKING=False
-ansible-playbook -vvvv -i ansible_deploy/inventory.yml ansible_deploy/deploy.yml --ask-sudo-pass
-unset ANSIBLE_HOST_KEY_CHECKING
---
+++ instagram.py
@@ -0,0 +1,21 @@
+from instagrapi import Client
+from pathlib import Path
+
+class SessionStorage:
+    def __init__(self):
+        self.SESSION_DIR = Path('''sessions''')
+        self.SESSION_DIR.mkdir(parents=True, exist_ok=True)
+
+    def get_session(self, session_name: str) -> Path:
+        return {self.SESSION_DIR} / Path(session_name)
+
+
+class InstagramModule:
+    def __init__(self, login: str, password: str):
+        self.client = Client()
+        self.session_storage = SessionStorage()
+        self.client.load_settings(self.session_storage.get_session(login))
+
+
+if __name__ == "__main__":
+    instagram_module = InstagramModule()
